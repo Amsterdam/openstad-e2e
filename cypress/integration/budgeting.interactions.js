053f5844810abd12f31485b8b3e1a770a319491b
Tosh Koevoets <tosh.koevoets@gmail.com>
2021-01-06 14:07:07 +0100

    Creating first tests (WIP)

---
+++ cypress/integration/budgeting.interactions.js
@@ -0,0 +1,19 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  console.log('Error ', err)
+  return false;
+})
+
+describe('Budgeting selecting ideas', () => {
+
+  it('Stuur een nieuw plan in', () => {
+
+    // goto site
+    cy.visit(Cypress.env('siteUrl'))
+
+    // check flassh message
+    cy.contains('Plannen')
+      .should('have.text', 'Dit doet nog niets')
+  })
+})
---
+++ cypress/integration/submitting.like-anonymous.js
@@ -0,0 +1,39 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
+
+describe('Anoniem liken', () => {
+
+  it('Ga naar plan en klik op like', () => {
+    // goto site
+    cy.visit(Cypress.env('submittingSiteUrl'))
+
+    // goto plannen
+    cy.contains('Plannen')
+      .click()
+
+    // click on the first plan
+    cy.get('.tile.idea-item.list-item')
+      .first().click()
+
+    // click on like button
+    cy.get('.role-required-anonymous.idea-status-OPEN.ajax-form')
+      .first().click()
+
+    // oauth postcode
+    cy.get('input.input-field')
+      .type('1011 HB')
+
+    // oauth submit
+    cy.get('.btn.btn-primary')
+      .click()
+
+    // check button
+    // @todo, change color to selected class
+    cy.get('.role-required-anonymous.idea-status-OPEN.ajax-form')
+      .should('have.css', 'background-color', 'rgb(189, 209, 49)');
+
+  })
+})
