c3f2cace6dcbb56bf41ac851fb0d5bff36b8cf0f
Niels Vegter <niels@denes.nl>
2022-09-06 10:01:00 +0200

    Remove 'Create a user as an moderator'

--- cypress/integration/submitting.moderator.js
+++ cypress/integration/submitting.moderator.js
@@ -24,48 +24,48 @@ const modBreak = `This is a modbreak ${new Date().getTime()}`
 
 describe('Actions of a moderator', () => {
 
-  it('Create a user as an moderator', () => {
-      // goto site
-      cy.loginModerator(Cypress.env('submittingSiteUrl'));
-
-      cy.visit(`${Cypress.env('submittingSiteUrl')}/admin`, {
-        auth: {
-            username: Cypress.env('adminBasicAuthUser'),
-            password: Cypress.env('adminBasicAuthPass')
-        }
-      })
-
-      const email = new Date().getTime() + 'user@' + Cypress.env('trustedEmailDomain');
-      const password = new Date().getTime() + Cypress.env('adminPassword');
-      const firstName = 'First Name '+ new Date().getTime();
-      const lastName = 'Last Name '+ new Date().getTime();
-      const postcode = 'Postcode '+ new Date().getTime();
-
-      cy.wait(1000);
-
-      cy.get('main a')
-        .contains('Gebruikers')
-        .first()
-        .click({force:true});
-
-      cy.get('a')
-        .contains('Create')
-        .first()
-        .click({force: true});
-
-      fillInField('email', email, 'text', cy);
-      fillInField('firstName', firstName, 'text', cy);
-      fillInField('lastName', lastName, 'text', cy);
-      fillInField('zipCode', postcode, 'text', cy);
-
-      // like the plan
-      cy.get('form button[type="submit"]')
-        .first()
-        .click();
-
-      cy.wait(200);
-
-  });
+  // it('Create a user as an moderator', () => {
+  //     // goto site
+  //     cy.loginModerator(Cypress.env('submittingSiteUrl'));
+  // 
+  //     cy.visit(`${Cypress.env('submittingSiteUrl')}/admin`, {
+  //       auth: {
+  //           username: Cypress.env('adminBasicAuthUser'),
+  //           password: Cypress.env('adminBasicAuthPass')
+  //       }
+  //     })
+  // 
+  //     const email = new Date().getTime() + 'user@' + Cypress.env('trustedEmailDomain');
+  //     const password = new Date().getTime() + Cypress.env('adminPassword');
+  //     const firstName = 'First Name '+ new Date().getTime();
+  //     const lastName = 'Last Name '+ new Date().getTime();
+  //     const postcode = 'Postcode '+ new Date().getTime();
+  // 
+  //     cy.wait(1000);
+  // 
+  //     cy.get('main a')
+  //       .contains('Gebruikers')
+  //       .first()
+  //       .click({force:true});
+  // 
+  //     cy.get('a')
+  //       .contains('Create')
+  //       .first()
+  //       .click({force: true});
+  // 
+  //     fillInField('email', email, 'text', cy);
+  //     fillInField('firstName', firstName, 'text', cy);
+  //     fillInField('lastName', lastName, 'text', cy);
+  //     fillInField('zipCode', postcode, 'text', cy);
+  // 
+  //     // like the plan
+  //     cy.get('form button[type="submit"]')
+  //       .first()
+  //       .click();
+  // 
+  //     cy.wait(200);
+  // 
+  // });
 
   it('Edit modbreak as a moderator', () => {
     cy.loginModerator(Cypress.env('submittingSiteUrl'));
